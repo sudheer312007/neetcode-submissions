948e5badeb7108e12f49369eef88e3d1150f1a1d
sudheer312007 <82172895+sudheer312007@users.noreply.github.com>
2026-06-07 14:21:46 +0530

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.java
@@ -0,0 +1,63 @@
+class Solution {
+      public void solve(char[][] board) {
+        boolean[][] visited = new boolean[board.length][board[0].length];
+
+        for (int i = 0; i < board.length; i++) {
+            if (!visited[i][0] && board[i][0] == 'O') {
+                solveHelper(i, 0, board, visited, false);
+            }
+
+            if (!visited[i][board[0].length - 1] && board[i][board[0].length - 1] == 'O') {
+                solveHelper(i, board[0].length - 1, board, visited, false);
+            }
+        }
+
+
+        for (int j = 0; j < board[0].length; j++) {
+            if (!visited[0][j] && board[0][j] == 'O') {
+                solveHelper(0, j, board, visited, false);
+            }
+
+            if (!visited[board.length - 1][j] && board[board.length - 1][j] == 'O') {
+                solveHelper(board.length - 1, j, board, visited, false);
+            }
+        }
+
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (!visited[i][j]) {
+                    solveHelper(i, j, board, visited, true);
+                }
+            }
+        }
+
+
+    }
+
+    private void solveHelper(int row, int col, char[][] board, boolean[][] visited, boolean markX) {
+        visited[row][col] = true;
+        if(markX){
+            board[row][col] = 'X';
+        }
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+
+        for (int[] direction : directions) {
+            int newRow = row + direction[0];
+            int newCol = col + direction[1];
+            if (canVisit(newRow, newCol, visited, board)) {
+                solveHelper(newRow, newCol, board, visited, markX);
+
+            }
+        }
+
+
+    }
+
+    private boolean canVisit(int row, int col, boolean[][] visited, char[][] grid) {
+        return row >= 0 && col >= 0 && row < visited.length && col < visited[0].length && !visited[row][col] && grid[row][col] == 'O';
+    }
+
+
+}
